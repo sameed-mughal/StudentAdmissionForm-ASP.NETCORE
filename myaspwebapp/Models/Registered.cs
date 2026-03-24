ab9fe018d38a61581dcf8cae2b6f218195cace2b
HannanJawed <abdulhannanjaw@gmail.com>
2026-03-24 12:37:14 +0500

    update Code

---
+++ myaspwebapp/Models/Registered.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace myaspwebapp.Models
+{
+    public class Registered
+    {
+
+        public string firstname {  get; set; }
+        public string lastname { get; set; }
+        public string gardianName { get; set; }
+        public string dob { get; set; }
+        public int age { get; set; }
+        public string gender { get; set; }
+        public string education { get; set; }
+        public string address { get; set; }
+        public string contact { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string interrestCourse { get; set; }
+        public string duration { get; set; }
+        public string reference { get; set; }
+
+    }
+}
---
+++ myaspwebapp/myaspwebapp/Controllers/RegisteredController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using myaspwebapp.Models;
+
+namespace myaspwebapp.Controllers
+{
+    public class RegisteredController : Controller
+    {
+        public IActionResult RegisterForm()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult RegisterForm(Registered registered)
+        {
+
+           ViewBag.Message = "Form has been Submitted Successfully";
+            ModelState.Clear();
+            return View();
+
+        }
+
+
+
+    }
+}
